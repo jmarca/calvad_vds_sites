9c0fa680f8efb44044baa41589c969b8265d49d2
James Marca <jmarca@translab.its.uci.edu>
2014-06-09 10:52:30 -0700

    test for vds query list raw imputed is passing
    can get zero for 2007 (not in test), 8 for 2010, and 514+ for 2014
    
    when I actually get 2014 data, I'll ahve to edit that test...

---
+++ test/test_get_vds_sites.js
@@ -0,0 +1,35 @@
+/* global require console process describe it */
+
+var should = require('should')
+
+var get_vds_need_raw_imputing = require('../lib/query_couchdb').get_vds_need_raw_imputing
+var config_okay = require('config_okay')
+
+var path    = require('path')
+var rootdir = path.normalize(__dirname)
+var config_file = rootdir+'/../test.config.json'
+var config={}
+
+
+describe('get sites',function(){
+    it('should get all the vds sites that need imputing',function(done){
+        get_vds_need_raw_imputing({'year':2010
+                                  ,'config_file':config_file}
+                                 ,function(e,r){
+                                      should.not.exist(e)
+                                      should.exist(r)
+                                      r.should.have.property('rows').with.lengthOf(8)
+                                      return done()
+                                  })
+    })
+    it('should get all the vds sites that need imputing',function(done){
+        get_vds_need_raw_imputing({'year':2014
+                                  ,'config_file':config_file}
+                                 ,function(e,r){
+                                      should.not.exist(e)
+                                      should.exist(r)
+                                      r.should.have.property('rows').with.lengthOf(519)
+                                      return done()
+                                  })
+    })
+})
