6de782ea7be13f4b8cf6938f4f0ee9f38935e01e
James E. Marca <jmarca@translab.its.uci.edu>
2014-06-09 10:38:47 -0700

    implement getting list of vds need raw imputed
    look at couchdb, get list, filter for nothing and unprocessed
    
    also a test
    
    not yet tested
    
    modified:   lib/query_couchdb.js
    modified:   test/test_get_wim_sites.js

--- test/test_get_wim_sites.js
+++ test/test_get_wim_sites.js
@@ -1,45 +1,25 @@
 /* global require console process describe it */
 
 var should = require('should')
-var wim_sites = require('../wim_sites')
+
+var get_vds_need_raw_imputing = require('../lib/query_couchdb').get_vds_need_Raw_imputing
+var config_okay = require('config_okay')
+
 var path    = require('path')
 var rootdir = path.normalize(__dirname)
-
-
 var config_file = rootdir+'/../test.config.json'
 var config={}
 
+
 describe('get sites',function(){
-    it('should get all the wim sites that need imputing',function(done){
-        wim_sites({'year':2007
-                  ,'config_file':config_file}
-                 ,function(e,r){
-                      should.not.exist(e)
-                      should.exist(r)
-                      //console.log(r)
-                      return done()
-        })
-    })
-    it('should get all the wim sites that need plotting',function(done){
-        wim_sites.get_wim_need_plotting({'year':2008
-                                        ,'config_file':config_file}
-                                       ,function(e,r){
-                                            should.not.exist(e)
-                                            should.exist(r)
-                                            r.should.have.property('rows').with.lengthOf(80)
-                                            //console.log(r)
-                                            return done()
-                                        })
-    })
-    it('should get all the wim sites that need pairing',function(done){
-        wim_sites.get_wim_need_pairing({'year':2007
-                                        ,'config_file':config_file}
-                                       ,function(e,r){
-                                            should.not.exist(e)
-                                            should.exist(r)
-                                            r.should.have.property('rows').with.lengthOf(86)
-                                            //console.log(r)
-                                            return done()
-                                        })
+    it('should get all the vds sites that need imputing',function(done){
+        get_vds_need_raw_imputing({'year':2007
+                                  ,'config_file':config_file}
+                                 ,function(e,r){
+                                      should.not.exist(e)
+                                      should.exist(r)
+                                      r.should.have.property('rows').with.lengthOf(4000)
+                                      return done()
+                                  })
     })
 })
