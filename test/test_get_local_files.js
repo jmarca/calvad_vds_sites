0ff9356d0ec2912daa5ec35c739c9967467606bf
James E. Marca <james@activimetrics.com>
2016-02-10 12:41:08 -0800

    implement get_files_local here
    fixes a problem in calvad_impute_missing
    
    tests pass.
    
    still need to implement and test the remote version

--- test/test_get_local_files.js
+++ test/test_get_local_files.js
@@ -1,65 +1,101 @@
-// test the db path
-
 /* global require console process describe it */
+var get_files = require('../.').get_yearly_vdsfiles_local
 
 var should = require('should')
+var path    = require('path')
+var rootdir = path.normalize(__dirname)
 
-var async = require('async')
-var _ = require('lodash')
-var superagent = require('superagent')
-var http = require('http')
 
-var get_files = require('../lib/get_files')
+var config_file = rootdir+'/../test.config.json'
+var config={}
+var config_okay = require('config_okay')
+before(function(done){
+
+    config_okay(config_file,function(err,c){
+        if(err){
+            throw new Error('node.js needs a good croak module')
+        }
+        config = c
+        return done()
+    })
+    return null
+})
+
 
 
 describe('get vds files local',function(){
-    it('should get txt files in 2010, D05'
-      ,function(done){
-           get_files.get_yearly_vdsfiles_local({'district':'D05'
-                                               ,'year':2010}
-                                              ,function(err,list){
-                                                   should.not.exist(err)
-                                                   //console.log(list)
-                                                   list.should.have.property('length',10)
-                                                   _.each(list
-                                                         ,function(f){
-                                                              f.should.match(/\.txt\.?.z$/);
-                                                          })
-                                                       return done()
-                                               })
+    it('should throw an error if vdspath is not defined'
+       ,function(){
+           var options = {'district':'D05'
+                          ,'year':2012};
+           // not setting calvad options here
+
+           (function(){
+               get_files(options
+                      ,function(err,list){
+                      })
+           }).should.throw()
+
        })
-    it('should get rdata files in 2007, D05'
-      ,function(done){
-           var yr = 2010
-           get_files.get_yearly_vdsfiles_local({'district':'D05'
-                                               ,'year':yr
-                                               ,'rdata':1}
-                                              ,function(err,list){
-                                                   should.not.exist(err)
-                                                   //console.log(list)
-                                                   list.should.have.property('length',10)
-                                                   _.each(list
-                                                         ,function(f){
-                                                              f.should.match(new RegExp(yr+'.*\.RData$'));
-                                                          })
-                                                       return done()
-                                               })
+    it('should get txt files in 2012, D05'
+       ,function(done){
+           var yr = 2012
+           var options = {'district':'D05'
+                          ,'year':yr
+                         }
+           options.calvad = config.calvad;
+
+           get_files(options
+                     ,function(err,list){
+                         should.not.exist(err)
+                         //console.log(list)
+                         list.should.have.property('length',8)
+                         list.forEach(function(f){
+                             f.should.match(/\.txt\.?.z$/);
+                             return null
+                         })
+                         return done()
+                     })
+           return null
        })
-    it('should get amelia files in 2010, D05'
-      ,function(done){
-           var yr = 2010
-           get_files.get_yearly_vdsfiles_local({'district':'D05'
-                                               ,'year':yr
-                                               ,'amelia':1}
-                                              ,function(err,list){
-                                                   should.not.exist(err)
-                                                   //console.log(list)
-                                                   list.should.have.property('length',10)
-                                                   _.each(list
-                                                         ,function(f){
-                                                              f.should.match(new RegExp(yr+'.*\.RData$'));
-                                                          })
-                                                       return done()
-                                               })
+    it('should get rdata files in 2012, D05'
+       ,function(done){
+           var yr = 2012
+           var options = {'district':'D05'
+                          ,'year':yr
+                          ,'rdata':1}
+           options.calvad = config.calvad
+           get_files(options
+                     ,function(err,list){
+                         should.not.exist(err)
+                         //console.log(list)
+                         list.should.have.property('length',8)
+                         list.forEach(function(f){
+                             f.should.match(new RegExp(yr+'.*\.RData$'));
+                             return null
+                         })
+                         return done()
+                     })
+           return null
+       })
+    it('should get amelia files in 2012, D05'
+       ,function(done){
+           var yr = 2012
+           var options = {'district':'D05'
+                          ,'year':yr
+                          ,'amelia':1}
+           options.calvad = config.calvad
+           get_files(options
+                      ,function(err,list){
+                          should.not.exist(err)
+                          //console.log(list)
+                          list.should.have.property('length',8)
+                          list.forEach(function(f){
+                              f.should.match(new RegExp(yr+'.*\.RData$'));
+                              return null
+                          })
+                          return done()
+                      })
+           return null
        })
 })
--- vds_sites.js
+++ vds_sites.js
@@ -1,9 +1,11 @@
 //var get_files = require('./lib/get_files')
 var q_c = require('./lib/query_couchdb')
-
+var locals = require('./lib/get_files_local.js')
 // exports.get_yearly_vdsfiles=get_files.get_yearly_vdsfiles
 //exports.get_yearly_vdsfiles_local=get_files.get_yearly_vdsfiles_local
 exports.is_not_imputed = q_c.is_not_imputed
 exports.get_vds_need_raw_imputing=q_c.get_vds_need_raw_imputing
 exports.get_vds_done_raw_imputing=q_c.get_vds_done_raw_imputing
 exports.get_vds_status_truckimputation = q_c.get_vds_status_truckimputation_imputing
+
+exports.get_yearly_vdsfiles_local = locals.get_yearly_vdsfiles_local
